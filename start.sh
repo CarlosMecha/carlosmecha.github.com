02f44f12957ea100eb89e57f204c8e0bd5d59060
Carlos Mecha <mecha.carlos@gmail.com>
2020-10-20 06:13:27 -0700

    Segment post (#2)
    * Add post about Segment
    
    * Finish post
    
    * Last update

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ docker run --rm \
     --volume "$PWD:/srv/jekyll" \
     --volume "$PWD/vendor/bundle:/usr/local/bundle" \
     -p "4000:4000" \
+    -p "35729:35729" \
     -it jekyll/jekyll:3.8 \
-    jekyll serve
+    jekyll serve --livereload
